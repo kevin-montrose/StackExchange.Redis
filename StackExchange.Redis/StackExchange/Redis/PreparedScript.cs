6a4e809ea4779a5c88ee0f5ba0a6a55d303148eb
kevin-montrose <kevin.montrose@gmail.com>
2015-02-02 21:22:22 -0500

    add a way to support running scripts by their hashes

--- StackExchange.Redis/StackExchange/Redis/PreparedScript.cs
+++ StackExchange.Redis/StackExchange/Redis/PreparedScript.cs
@@ -56,10 +56,11 @@ namespace StackExchange.Redis.StackExchange.Redis
         /// </summary>
         public static PreparedScript Prepare(string script)
         {
+            // It's all the same, no reason to do the work twice.
             return Cache.GetOrAdd(script, _ => ScriptParameterMapper.PrepareScript(script));
         }
 
-        void ExtractParameters(object ps, out RedisKey[] keys, out RedisValue[] args)
+        internal void ExtractParameters(object ps, out RedisKey[] keys, out RedisValue[] args)
         {
             if (HasArguments)
             {
@@ -113,6 +114,98 @@ namespace StackExchange.Redis.StackExchange.Redis
             ExtractParameters(ps, out keys, out args);
 
             return db.ScriptEvaluateAsync(ExecutableScript, keys, args, flags);
+        }
+
+        /// <summary>
+        /// Loads this PreparedScript into the given IServer so it can be run with it's SHA1 hash, instead of
+        /// passing the full script on each Evaluate or EvaluateAsync call.
+        /// </summary>
+        public LoadedPreparedScript ScriptLoad(IServer server, CommandFlags flags = CommandFlags.None)
+        {
+            var hash = server.ScriptLoad(ExecutableScript, flags);
+
+            return new LoadedPreparedScript(this, hash);
+        }
+
+        /// <summary>
+        /// Loads this PreparedScript into the given IServer so it can be run with it's SHA1 hash, instead of
+        /// passing the full script on each Evaluate or EvaluateAsync call.
+        /// </summary>
+        public async Task<LoadedPreparedScript> ScriptLoadAsync(IServer server, CommandFlags flags = CommandFlags.None)
+        {
+            var hash = await server.ScriptLoadAsync(ExecutableScript, flags);
+
+            return new LoadedPreparedScript(this, hash);
+        }
+    }
+
+    /// <summary>
+    /// Represents a Lua script that can be executed on Redis.
+    /// 
+    /// Unlike PreparedScript, LoadedPreparedScript sends the hash of it's ExecutableScript to Redis rather than pass
+    /// the whole script on each call.  This requires that the script be loaded into Redis before it is used.
+    /// 
+    /// To create a LoadedPreparedScript first create a PreparedScript via PreparedScript.Prepare(string), then
+    /// call Load(IServer, CommandFlags) on the returned PreparedScript.
+    /// 
+    /// Unlike normal Redis Lua scripts, LoadedPreparedScript can have named parameters (prefixed by a @).
+    /// Public fields and properties of the passed in object are treated as parameters.
+    /// 
+    /// Parameters of type RedisKey are sent to Redis as KEY (http://redis.io/commands/eval) in addition to arguments, 
+    /// so as to play nicely with Redis Cluster.
+    /// 
+    /// All members of this class are thread safe.
+    /// </summary>
+    public sealed class LoadedPreparedScript
+    {
+        /// <summary>
+        /// The original script that was used to create this PreparedScript.
+        /// </summary>
+        public string OriginalScript { get { return Original.OriginalScript; } }
+
+        /// <summary>
+        /// The script that will actually be sent to Redis for execution.
+        /// </summary>
+        public string ExecutableScript { get { return Original.ExecutableScript; } }
+
+        public byte[] Hash { get; private set; }
+
+        PreparedScript Original;
+
+        internal LoadedPreparedScript(PreparedScript original, byte[] hash)
+        {
+            Original = original;
+            Hash = hash;
+        }
+
+        /// <summary>
+        /// Evaluates this LoadedPreparedScript against the given database, extracting parameters for the passed in object if any.
+        /// 
+        /// This method sends the SHA1 hash of the ExecutableScript instead of the script itself.  If the script has not
+        /// been loaded into the passed Redis instance it will fail.
+        /// </summary>
+        public RedisResult Evaluate(IDatabase db, object ps = null, CommandFlags flags = CommandFlags.None)
+        {
+            RedisKey[] keys;
+            RedisValue[] args;
+            Original.ExtractParameters(ps, out keys, out args);
+
+            return db.ScriptEvaluate(Hash, keys, args, flags);
+        }
+
+        /// <summary>
+        /// Evaluates this LoadedPreparedScript against the given database, extracting parameters for the passed in object if any.
+        /// 
+        /// This method sends the SHA1 hash of the ExecutableScript instead of the script itself.  If the script has not
+        /// been loaded into the passed Redis instance it will fail.
+        /// </summary>
+        public Task<RedisResult> EvaluateAsync(IDatabaseAsync db, object ps = null, CommandFlags flags = CommandFlags.None)
+        {
+            RedisKey[] keys;
+            RedisValue[] args;
+            Original.ExtractParameters(ps, out keys, out args);
+
+            return db.ScriptEvaluateAsync(Hash, keys, args, flags);
         }
     }
 }
