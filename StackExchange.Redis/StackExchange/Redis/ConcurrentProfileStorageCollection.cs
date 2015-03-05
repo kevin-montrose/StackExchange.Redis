2420ec11cca888e22aaa2dd525faafb71affb9d1
kevin-montrose <kevin.montrose@gmail.com>
2015-03-05 16:48:49 -0500

    add low overhead, low allocation, low risk profiling hooks

---
+++ StackExchange.Redis/StackExchange/Redis/ConcurrentProfileStorageCollection.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StackExchange.Redis
+{
+    /// <summary>
+    /// A thread-safe collection tailored to the "always append, with high contention, then enumerate once with no contention"
+    /// behavior of our profiling.
+    /// 
+    /// Performs better than ConcurrentBag, which is important since profiling code shouldn't impact timings.
+    /// </summary>
+    sealed class ConcurrentProfileStorageCollection
+    {
+        // internal for test purposes
+        internal static int AllocationCount = 0;
+
+        // It is, by definition, impossible for an element to be in 2 intrusive collections
+        //   and we force Enumeration to release any reference to the collection object
+        //   so we can **always** pool these (by type).
+        const int PoolSize = 64;
+        static ConcurrentProfileStorageCollection[] Pool = new ConcurrentProfileStorageCollection[PoolSize];
+
+        volatile ProfileStorage Head;
+
+        private ConcurrentProfileStorageCollection() { }
+
+        /// <summary>
+        /// This method is thread-safe.
+        /// 
+        /// Adds an element to the bag.
+        /// 
+        /// Order is not preserved.
+        /// 
+        /// The element can only be a member of *one* bag.
+        /// </summary>
+        public void Add(ProfileStorage command)
+        {
+            do
+            {
+                var cur = Head;
+                command.NextElement = cur;
+
+                // Interlocked references to volatile fields are perfectly cromulent
+#pragma warning disable 420
+                var got = Interlocked.CompareExchange(ref Head, command, cur);
+#pragma warning restore 420
+
+                if (object.ReferenceEquals(got, cur)) break;
+            } while (true);
+        }
+
+        // Seperate method to ensure that no *this* is captured with all these `yield`-shenanigans.
+        // This is not technically necessary, given a close reading of the C# guarantees... but
+        //   relying on everyone to remember that is perhaps a bit much.
+        static IEnumerable<ProfileStorage> MakeEnumerable(ProfileStorage head)
+        {
+            // This is implemented as a lazy enumerable
+            //   so that there's only one, relatively small, allocation. 
+            //   (example generated class can be found here: http://csharpindepth.com/articles/chapter6/iteratorblockimplementation.aspx )
+            // Turning it into a List or array feels wasteful.
+            var cur = head;
+            while (cur != null)
+            {
+                yield return cur;
+                cur = cur.NextElement;
+            }
+        }
+
+        /// <summary>
+        /// This method returns an enumerable view of the bag.
+        /// 
+        /// It is not thread safe.
+        /// 
+        /// It should only be called once the bag is finished being mutated.
+        /// </summary>
+        public IEnumerable<ProfileStorage> EnumerateAndReturnForReuse()
+        {
+            var ret = MakeEnumerable(Head);
+
+            // no need for interlocking, this isn't a thread safe method
+            Head = null;
+
+            for (var i = 0; i < PoolSize; i++)
+            {
+                if (Interlocked.CompareExchange(ref Pool[i], this, null) == null) break;
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public static ConcurrentProfileStorageCollection GetOrCreate()
+        {
+            ConcurrentProfileStorageCollection found;
+            for (int i = 0; i < PoolSize; i++)
+            {
+                if ((found = Interlocked.Exchange(ref Pool[i], null)) != null)
+                {
+                    return found;
+                }
+            }
+
+            Interlocked.Increment(ref AllocationCount);
+            found = new ConcurrentProfileStorageCollection();
+
+            return found;
+        }
+    }
+}
